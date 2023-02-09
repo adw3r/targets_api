c71369c157de10740560e1316675984275a78a48
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-02-09 19:02:55 +0200

    init

--- run_api.cmd
+++ run_api.cmd
@@ -1,3 +1,1 @@
-docker build -t targets_api .
-docker kill targets_api
-docker run -d --rm -v C:\Users\Administrator\Desktop\targets_api:/myapp --env-file .env --name targets_api -p 8181:8181 targets_api
+venv\Scripts\python.exe main.py
