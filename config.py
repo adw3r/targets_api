ed98086c0b29680d6f10956e7339c3b88bfc0187
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-02-10 16:34:44 +0200

    init

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
+import configparser
 import os
 import pathlib
 
-from dotenv import load_dotenv
-
-load_dotenv()
 PACKAGE_FOLDER = pathlib.Path(__file__).parent
 TARGETS_FOLDER = pathlib.Path(PACKAGE_FOLDER, 'targets')
 if not TARGETS_FOLDER.exists():
     os.mkdir(TARGETS_FOLDER)
 
-HOST = os.environ.get('HOST', '0.0.0.0')
-PORT = os.environ.get('PORT', int('8181'))
+config = configparser.ConfigParser()
+config.read(pathlib.Path(PACKAGE_FOLDER, 'config.ini'))
+
+HOST = config['general'].get('HOST', 'localhost')
+PORT = config['general'].getint('PORT', 8181)
