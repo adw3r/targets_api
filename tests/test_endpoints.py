dc9439176bdf51a5abb33f1433147a7ff99bbd85
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-02-24 14:17:10 +0200

    fiex

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -7,7 +7,7 @@ from config import PORT
 
 
 def check_target(_):
-    resp = httpx.get('http://localhost:%s/targets/g11mp2' % PORT, params={'method': 'pop'}, timeout=60).text
+    resp = httpx.get('http://localhost:%s/targets/test' % PORT, params={'method': 'pop'}, timeout=60).text
     print(resp)
     return '@' in resp
 
@@ -17,24 +17,26 @@ class TestEndpoints(TestCase):
     def test_status(self):
         resp = httpx.get('http://localhost:%s' % PORT)
         success = resp.status_code
-        self.assertTrue(success < 400)
+        success_ = success < 400
+        print(success_)
+        self.assertTrue(success_)
 
     def test_targets_pool(self):
         params = {'method': 'pool', 'limit': '10'}
-        resp = httpx.get('http://localhost:%s/targets/turk' % PORT, params=params).text.splitlines()
+        resp = httpx.get('http://localhost:%s/targets/test' % PORT, params=params).text.splitlines()
         for line in resp:
             print(line)
             self.assertTrue('@' in line)
 
     def test_get_info(self):
         params = {'method': 'info'}
-        resp = httpx.get('http://localhost:%s/targets/turk' % PORT, params=params).json()
+        resp = httpx.get('http://localhost:%s/targets/test' % PORT, params=params).json()
         print(resp)
         self.assertTrue(resp['lang'] is not None)
         self.assertTrue(resp['amount'] is not None)
 
     def test_pop_target(self):  # 4 multithreaded
-        threads_amount = 50
+        threads_amount = 20
         with ThreadPoolExecutor(threads_amount) as worker:
             results = [res for res in worker.map(check_target, [_ for _ in range(threads_amount)])]
         for res in results:
