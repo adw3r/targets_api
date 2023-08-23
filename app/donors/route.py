27f3ef8814c1062b0f7d2d89e579b93186569c2d
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-08-23 11:17:35 +0300

    Revert "slightly changed status changing flow"
    This reverts commit 48bd3598fa8876c5b6faf02a29a1a35350d4e7a0.

--- app/donors/route.py
+++ app/donors/route.py
@@ -11,21 +11,17 @@ router = APIRouter(
 )
 
 
-async def status_control(db_session: AsyncSession, donor: models.SpamDonor):
-    if donor.fail_count <= config.FAILS_LIMIT and donor.status is True:
-        donor.status = False
-        db_session.add(donor)
-        await db_session.commit()
-        await db_session.refresh(donor)
-
-
 @router.get('/{donor_name}')
 async def get_project_info(donor_name: str, db_session: AsyncSession = Depends(database.create_async_session)):
     donor: models.SpamDonor = await service.get_donor_by_name(db_session, donor_name)
     if not donor:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail='Donor with name {} not found'.format(donor_name))
-    await status_control(db_session, donor)
+    if donor.fail_count <= config.FAILS_LIMIT and donor.status is True:
+        donor.status = False
+        db_session.add(donor)
+        await db_session.commit()
+        await db_session.refresh(donor)
     return donor
 
 
@@ -65,10 +61,8 @@ async def send_count(donor_name: str, json_form: schemas.SpamDonorCount,
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'donor {donor_name} was not found!')
     if json_form.success_count >= 0:
         donor_instance.success_count += json_form.success_count
-        donor_instance.status = True
     else:
         donor_instance.fail_count += json_form.success_count
-        await status_control(db_session, donor_instance)
     db_session.add(donor_instance)
     await db_session.commit()
     return Response(content='success')
