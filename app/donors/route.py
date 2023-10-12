3db2eeee8762eb328fa64020361b777b28b56439
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-10-12 16:39:57 +0300

    fixed status fail control flow

--- app/donors/route.py
+++ app/donors/route.py
@@ -10,43 +10,39 @@ router = APIRouter(
 )
 
 
-async def status_control(db_session: AsyncSession, donor: models.SpamDonor):
-    if donor.fail_count <= config.FAILS_LIMIT and donor.status is True:
-        donor.status = False
-        db_session.add(donor)
-        await db_session.commit()
-        await db_session.refresh(donor)
-
-
 @router.get('/')
 async def get_donors(db_session: AsyncSession = Depends(database.create_async_session)):
     results = await donors.service.get_all_donors(db_session)
     return results
 
 
-@router.get('/{donor_name}')
-async def get_project_info(donor_name: str, db_session: AsyncSession = Depends(database.create_async_session)):
-    donor: models.SpamDonor = await service.get_donor_by_name(db_session, donor_name)
-    if not donor:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail='Donor with name {} not found'.format(donor_name))
-    await status_control(db_session, donor)
-    return donor
-
-
-@router.post('')
+@router.post('/')
 async def create_project(donor_scheme: schemas.SpamDonorPostSchema,
                          db_session: AsyncSession = Depends(database.create_async_session)):
     donor_instance: models.SpamDonor = await service.get_donor_by_name(db_session, donor_scheme.donor_name)
     if donor_instance:
-        donor_instance.update(**donor_scheme.dict())
+        donor_instance.update(**donor_scheme.model_dump())
         db_session.add(donor_instance)
         await db_session.commit()
     else:
-        donor_instance = models.SpamDonor(**donor_scheme.dict())
+        donor_instance = models.SpamDonor(**donor_scheme.model_dump())
         db_session.add(donor_instance)
         await db_session.commit()
     await db_session.refresh(donor_instance)
+    return donor_instance
+
+
+@router.get('/{donor_name}')
+async def get_project_info(donor_name: str, db_session: AsyncSession = Depends(database.create_async_session)):
+    donor_instance: models.SpamDonor = await service.get_donor_by_name(db_session, donor_name)
+    if not donor_instance:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='Donor with name {} not found'.format(donor_name))
+    if donor_instance.fail_count >= config.FAILS_LIMIT and donor_instance.status is True:
+        donor_instance.status = False
+        db_session.add(donor_instance)
+        await db_session.commit()
+        await db_session.refresh(donor_instance)
     return donor_instance
 
 
@@ -54,6 +50,8 @@ async def create_project(donor_scheme: schemas.SpamDonorPostSchema,
 async def update_status(donor_name: str, status: bool,
                         db_session: AsyncSession = Depends(database.create_async_session)):
     donor_instance: models.SpamDonor = await service.get_donor_by_name(db_session, donor_name)
+    if not donor_instance:
+        raise HTTPException(detail=f'donor {donor_name} was not found!', status_code=404)
     donor_instance.status = status
     donor_instance.fail_count = 0
     db_session.add(donor_instance)
@@ -68,12 +66,17 @@ async def send_count(donor_name: str, json_form: schemas.SpamDonorCount,
     donor_instance = await service.get_donor_by_name(db_session, donor_name)
     if not donor_instance:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'donor {donor_name} was not found!')
-    if json_form.success_count >= 0:
+    if json_form.success_count > 0:
         donor_instance.success_count += json_form.success_count
+        donor_instance.fail_count = 0
         donor_instance.status = True
+    elif json_form.success_count == 0:
+        pass
     else:
-        donor_instance.fail_count += json_form.success_count
-        await status_control(db_session, donor_instance)
+        donor_instance.fail_count += abs(json_form.success_count)
+        if donor_instance.fail_count >= config.FAILS_LIMIT:
+            donor_instance.status = False
     db_session.add(donor_instance)
     await db_session.commit()
+    await db_session.refresh(donor_instance)
     return Response(content='success')
